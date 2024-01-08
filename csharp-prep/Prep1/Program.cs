e9bc8145dc44dbc6f02b011e0ae2507457b69f01
Sleeponit-Thurs <jasew.erickson@gmail.com>
2024-01-08 14:08:56 -0700

    Completed prep one and two

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Please enter your first name. ");
+        string NameFirst = Console.ReadLine();
+        Console.WriteLine("Please enter your last name. ");
+        string NameLast = Console.ReadLine();
+        Console.WriteLine($"Your name is {NameFirst + " " + NameLast}");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        ///This Program will take a grade input in a value from 0-100 and output a letter grade A-F
+        string GradeInput;
+        int GradeNumber;
+        string GradeLetter;
+        while (true)
+        {
+            Console.WriteLine("What is your grade from 0-100?");
+            GradeInput = Console.ReadLine();
+            if (int.TryParse(GradeInput, out GradeNumber) && (GradeNumber > 0) && (GradeNumber < 100))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please give a valid Input");
+            }
+                
+            
+        }
+        GradeNumber = int.Parse(GradeInput);
+        GradeLetter = "A";
+        if (GradeNumber < 90)
+            GradeLetter = "B";
+        if (GradeNumber < 80)
+            GradeLetter = "C";
+        if (GradeNumber < 70)
+            GradeLetter = "D";
+        if (GradeNumber < 60)
+            GradeLetter = "F";
+        
+        Console.WriteLine($"{GradeLetter} is your grade.");
+            
+        
     }
 }
