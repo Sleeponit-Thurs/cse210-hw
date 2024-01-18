3465ce2ec859621c4cfe610acbbc21028d0caa3c
Sleeponit-Thurs <jasew.erickson@gmail.com>
2024-01-17 19:29:36 -0700

    Number Game Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int tries = 0;
+        while (true)
+        {
+        Random randomNumberGen = new Random();
+        int magicNumber = randomNumberGen.Next(1,100);
+        if (tries != 0)
+        {
+            Console.WriteLine("Would you like to play again? y/n");
+            string response = Console.ReadLine();
+            if (response == "y")
+            {
+                tries = 0;
+                continue;
+            }
+            else if (response == "n")
+            {
+                Console.WriteLine("Thanks for playing!");
+                break;
+            }
+        }
+        
+            while (true)
+            {
+                tries += 1;
+                Console.WriteLine("Guess a number!");
+                int guess = int.Parse(Console.ReadLine());
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess == magicNumber)
+                {
+                    Console.WriteLine($"You guessed it! It took {tries} tries.");
+                    break;
+                }
+
+                
+            }
+        }
+
+        
     }
 }
