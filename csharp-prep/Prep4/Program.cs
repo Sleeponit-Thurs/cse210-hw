bd4561b387b8df546cab144fe25597eeb11acaa7
Sleeponit-Thurs <jasew.erickson@gmail.com>
2024-01-17 21:19:25 -0700

    FInished prep 4 and 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numbers = new List<float>();
+        float numberAdd = 1;
+        Console.WriteLine("Enter a list of numbers. Type zero to stop.");
+        while (numberAdd != 0)
+        {
+            //Accepts input until 0 is given
+            Console.Write("Enter a number: ");
+            string numberConvert = Console.ReadLine();
+            numberAdd = float.Parse(numberConvert);
+            if (numberAdd != 0)
+            {
+                numbers.Add(numberAdd);
+            }
+            
+        }
+        float sum = numbers.Sum();
+        float average = sum / numbers.Count;
+        float largestNumber = numbers.Max();
+        float smallestNumber = numbers.Min();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest number is: {smallestNumber}");
+        Console.WriteLine($"Here are the numbers from least to greatest:");
+        numbers.Sort();
+        foreach (float number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string UserName = Console.ReadLine();
+            return UserName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            int FavoriteNumber = Console.Read();
+            return FavoriteNumber;
+        }
+        static float SquareNumber(float value)
+        {
+            float numberSquared = value * value;
+            return numberSquared;
+        }
+        static void displayResult(string name, float number)
+        {
+            Console.WriteLine($"{name}, the square of your number is {number}");
+        }
+        DisplayWelcome();
+        string userName = PromptUserName();
+        float favNumber = PromptUserNumber();
+        float favNumberSquared = SquareNumber(favNumber);
+        displayResult(userName, favNumberSquared);
+
+
     }
 }
