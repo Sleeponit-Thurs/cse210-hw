db7aad03406b5d085bd47db205044a152c8ab236
Sleeponit-Thurs <jasew.erickson@gmail.com>
2024-02-21 14:48:38 -0700

    Learning 4 Submission

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Jase Erickson", "Mathematics");
+        assignment1.GetSummary();
+
+        MathAssignment assignment2 = new MathAssignment("Quandale Dingle", "Pizza", "9.2", "14-78");
+        assignment2.GetSummary();
+        assignment2.GetHomeworkList();
+
+        WritingAssignment assignment3 = new WritingAssignment("Joe Mama", "Video Games", "Kirby's Fantastic Adventure");
+        assignment3.GetSummary();
+        assignment3.GetWritingInformation();
+
     }
 }
